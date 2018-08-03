ce5dc86b546babd68a060b0066c9ecafbbc8ec42
Erin Doyle <edoyle@cashstar.com>
2018-08-02 20:38:48 -0400

    Added basic navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import MovieWishlist from './wishlist/MovieWishlist';
+import MovieBrowser from './browse/MovieBrowser';
+
+
+const App = () => (
+    <div>
+        <Switch>
+            <Redirect exact from='/' to='/wishlist' />
+            <Route path="/wishlist" component={MovieWishlist} />
+            <Route path="/browse" component={MovieBrowser} />
+        </Switch>
+    </div>
+);
 
 export default App;
