07f7387755e1261abf579c3c8836dc7ea78341b7
Erin Doyle <edoyle@cashstar.com>
2018-08-05 11:46:03 -0400

    Added ability to add/edit notes for a Movie with a modal

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ ReactDOM.render((
     <BrowserRouter>
         <App/>
     </BrowserRouter>
-), document.getElementById('root'));
+), document.getElementById('app-root'));
 
 registerServiceWorker();
